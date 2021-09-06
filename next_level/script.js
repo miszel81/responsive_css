8e2ed0153db9e02e29536d1a4946414bc7ff3064
michal <miszel81@gmail.com>
2021-09-06 23:27:18 +0200

    a lot of changes

---
+++ next_level/script.js
@@ -0,0 +1,11 @@
+const closeButton = document.querySelector('.close-nav');
+const openButton = document.querySelector('.open-nav');
+const nav = document.querySelector('.nav');
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('nav-open');
+});
+
+openButton.addEventListener('click', () => {
+  nav.classList.add('nav-open');
+});
